c709f6fc18021e0f6fcff90af10ab76232f54e34
Orlando Quero <orlaqp@bincrafters.com>
2023-03-19 13:02:45 -0400

    add some tests for lambda microservice

--- e2e/nx-aws-cdk-e2e/tests/nx-aws-cdk.lambda-microservice.spec.ts
+++ e2e/nx-aws-cdk-e2e/tests/nx-aws-cdk.lambda-microservice.spec.ts
@@ -1,58 +1,58 @@
-// import {
-//   checkFilesExist,
-//   ensureNxProject,
-//   readJson,
-//   runNxCommandAsync,
-//   uniq,
-// } from '@nrwl/nx-plugin/testing';
+import {
+  checkFilesExist,
+  ensureNxProject,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
 
-// describe('nx-aws-cdk lambda api e2e', () => {
-//   // Setting up individual workspaces per
-//   // test can cause e2e runs to take a long time.
-//   // For this reason, we recommend each suite only
-//   // consumes 1 workspace. The tests should each operate
-//   // on a unique project in the workspace, such that they
-//   // are not dependant on one another.
-//   beforeAll(() => {
-//     ensureNxProject('@bincrafters/nx-aws-cdk', 'dist/packages/nx-aws-cdk');
-//   });
+describe('nx-aws-cdk lambda microservice e2e', () => {
+  // Setting up individual workspaces per
+  // test can cause e2e runs to take a long time.
+  // For this reason, we recommend each suite only
+  // consumes 1 workspace. The tests should each operate
+  // on a unique project in the workspace, such that they
+  // are not dependant on one another.
+  beforeAll(() => {
+    ensureNxProject('@bincrafters/nx-aws-cdk', 'dist/packages/nx-aws-cdk');
+  });
 
-//   afterAll(() => {
-//     // `nx reset` kills the daemon, and performs
-//     // some work which can help clean up e2e leftovers
-//     runNxCommandAsync('reset');
-//   });
+  afterAll(() => {
+    // `nx reset` kills the daemon, and performs
+    // some work which can help clean up e2e leftovers
+    runNxCommandAsync('reset');
+  });
 
-//   it('should create app', async () => {
-//     const project = uniq('nx-aws-cdk');
-//     await runNxCommandAsync(
-//       `generate @bincrafters/nx-aws-cdk:lambda-microservice ${project}`
-//     );
-//     const result = await runNxCommandAsync(`build ${project}`);
-//     expect(result.stdout).toContain('Executor ran');
-//   }, 120000);
+  // it('should create app', async () => {
+  //   const project = uniq('nx-aws-cdk');
+  //   await runNxCommandAsync(
+  //     `generate @bincrafters/nx-aws-cdk:lambda-microservice ${project}`
+  //   );
+  //   const result = await runNxCommandAsync(`build ${project}`);
+  //   expect(result.stdout).toContain('Executor ran');
+  // }, 120000);
 
-//   describe('--directory', () => {
-//     it('should create src in the specified directory', async () => {
-//       const project = uniq('nx-aws-cdk');
-//       await runNxCommandAsync(
-//         `generate @bincrafters/nx-aws-cdk:app ${project} --directory subdir`
-//       );
-//       expect(() =>
-//         checkFilesExist(`apps/subdir/${project}/src/app.ts`)
-//       ).not.toThrow();
-//     }, 120000);
-//   });
+  describe('--directory', () => {
+    it('should create src in the specified directory', async () => {
+      const project = uniq('nx-aws-cdk');
+      await runNxCommandAsync(
+        `generate @bincrafters/nx-aws-cdk:lambda-microservice ${project} --directory subdir`
+      );
+      expect(() =>
+        checkFilesExist(`apps/subdir/${project}/src/app.ts`)
+      ).not.toThrow();
+    }, 120000);
+  });
 
-//   describe('--tags', () => {
-//     it('should add tags to the project', async () => {
-//       const projectName = uniq('nx-aws-cdk');
-//       ensureNxProject('@bincrafters/nx-aws-cdk', 'dist/packages/nx-aws-cdk');
-//       await runNxCommandAsync(
-//         `generate @bincrafters/nx-aws-cdk:app ${projectName} --tags e2etag,e2ePackage`
-//       );
-//       const project = readJson(`apps/${projectName}/project.json`);
-//       expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
-//     }, 120000);
-//   });
-// });
+  describe('--tags', () => {
+    it('should add tags to the project', async () => {
+      const projectName = uniq('nx-aws-cdk');
+      ensureNxProject('@bincrafters/nx-aws-cdk', 'dist/packages/nx-aws-cdk');
+      await runNxCommandAsync(
+        `generate @bincrafters/nx-aws-cdk:lambda-microservice ${projectName} --tags e2etag,e2ePackage`
+      );
+      const project = readJson(`apps/${projectName}/project.json`);
+      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+    }, 120000);
+  });
+});
